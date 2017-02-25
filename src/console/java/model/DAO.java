95cb0b85f2a2824bf2f631a6d7b3ce746a5b778e
Tuan Anh <tuana@gmail.com>
2017-02-25 19:31:17 +0700

    sua lai DAO

--- src/console/java/model/DAO.java
+++ src/console/java/model/DAO.java
@@ -14,18 +14,11 @@ import java.sql.SQLException;
  * @author tuana
  */
 public class DAO {
-    public static final String CONNECTION_URL_PREFIX = "jdbc:mysql://" ;
-    public static final String DATABASE = "products";
-    public static final String DATABASE_USER = "?user=root";
-    public static final String DATABASE_PASSWORD = "";
-    public static final String HOST_WITH_HOST = "//localhost:3306/";
-    
-    public static Connection getConnection()throws SQLException{
-        String connectionString = CONNECTION_URL_PREFIX + HOST_WITH_HOST + DATABASE + DATABASE_USER + DATABASE_PASSWORD;
+
+    public static Connection getConnection() throws SQLException {
+     
+        String connectionString = GlobalConfig.getCONNECTION_URL_PREFIX() + GlobalConfig.getDATABASE() + GlobalConfig.getHOST() + GlobalConfig.getUSERNAME() + GlobalConfig.getPASSWORD();
         Connection conn = DriverManager.getConnection(connectionString);
         return conn;
-    
     }
-    
-    
 }
