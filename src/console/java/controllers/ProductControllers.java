4842f878716b673615b64a6e59d1ced4c449a97a
Thang <anhthang3011@gmail.com>
2017-02-27 11:35:51 +0700

    Update

--- src/console/java/controllers/ProductControllers.java
+++ src/console/java/controllers/ProductControllers.java
@@ -6,11 +6,13 @@
 package console.java.controllers;
 
 import console.java.entity.Product;
+import console.java.model.DAO;
 import console.java.model.ProductModels;
 import console.java.utilities.ScannerUtilities;
 import console.java.views.ProductViews;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Class này chứa các hàm xử lý sản phẩm
@@ -54,5 +56,59 @@ public class ProductControllers {
             }
             continueBoolean = ProductViews.continueBoolean();
         }
+    }
+
+    public static boolean processUpdate() {
+        try {
+                int count = 0;
+                System.out.println("Nhap ma so : ");
+                String barCode = ScannerUtilities.getString();
+                Statement stt = DAO.getConnection().createStatement();
+
+                String sql = "SELECT * FROM products WHERE barcode = '" + barCode + "'";
+                ResultSet rs = stt.executeQuery(sql);
+
+                while (rs.next()) {
+                    String name = rs.getString("name");
+                    String description = rs.getString("description");
+                    String quantity = rs.getString("quantity");
+                    String price = rs.getString("price");
+                    String categoryId = rs.getString("categoryId");
+                    System.out.printf("%-10s %s\n", "Ten         :", name);
+                    System.out.printf("%-10s %s\n", "Mo ta       :", description);
+                    System.out.printf("%-10s %s\n", "So luong    :", quantity);
+                    System.out.printf("%-10s %s\n", "Gia         :", price);
+                    System.out.printf("%-10s %s\n", "Chung loai  :", categoryId);
+                    ++count;
+                }
+                
+                if (count == 0) {
+                    System.err.println("Khong co san pham !!!");
+                    return false;
+                }
+
+                System.out.println("Chu y : Neu ban khong muon sua ,hay de trong va tiep tuc !!!");
+                System.out.println("Nhap ten moi           :");
+                String newName = ScannerUtilities.getString();
+                System.out.println("Nhap mo ta moi         :");
+                String newDescription = ScannerUtilities.getString();
+                System.out.println("Nhap so luong moi      :");
+                int newQuantity = ScannerUtilities.getInt();
+                System.out.println("Nhap gia moi           :");
+                float newPrice = ScannerUtilities.getFloat();
+                System.out.println("Nhap ma chung loai moi :");
+                int newCategoryId = ScannerUtilities.getInt();
+
+                Product product = new Product();
+                product.setName(newName);
+                product.setDescription(newDescription);
+                product.setQuantity(newQuantity);
+                product.setPrice(newPrice);
+                product.setCategoryId(newCategoryId);
+                //Call update method in productModel class
+        } catch (SQLException e) {
+            System.err.println("Khong the update !!!");
+        }
+        return true;
     }
 }
--- src/console/java/utilities/ScannerUtilities.java
+++ src/console/java/utilities/ScannerUtilities.java
@@ -31,6 +31,19 @@ public class ScannerUtilities {
         }
         return inputInt;
     }
+    
+    public static float getFloat() {
+        float inputInt = 0;
+        while (true) {
+            try {
+                inputInt = new Scanner(System.in).nextFloat();
+                break;
+            } catch (Exception e) {
+                System.err.println("Vui lòng chỉ nhập số! ");
+            }
+        }
+        return inputInt;
+    }
 
     /**
      * Hàm này trả về một chuỗi mà người dùng nhập vào từ bàn phím
