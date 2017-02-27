c9f549bff5002eecf0e80e81c0b71b02754cf9a5
NguyenManhTruong <nguyenmanhtruong0@gmail.com>
2017-02-27 20:03:54 +0700

    Sửa lại table admin

--- Database/admin.sql
+++ Database/admin.sql
@@ -1,37 +1,60 @@
-USE [admin]
-GO
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 27, 2017 at 02:02 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
-/****** Object:  Table [dbo].[admins]    Script Date: 2/27/2017 3:25:09 PM ******/
-DROP TABLE [dbo].[admins]
-GO
-
-/****** Object:  Table [dbo].[admins]    Script Date: 2/27/2017 3:25:09 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-SET ANSI_PADDING ON
-GO
-
-CREATE TABLE [dbo].[admins](
-	[id] [int] IDENTITY(1,1) NOT NULL,
-	[name] [varchar](60) NOT NULL,
-	[email] [varchar](60) NOT NULL,
-	[passwords] [varchar](20) NOT NULL,
-	[status] [int] NOT NULL,
-	[creat_at] [datetime] NOT NULL CONSTRAINT [DF_admins_creat_at]  DEFAULT (getdate()),
-	[update_at] [datetime] NULL CONSTRAINT [DF__admins__update_a__108B795B]  DEFAULT (getdate()),
-PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-
-GO
-
-SET ANSI_PADDING OFF
-GO
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `product`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `pass` varchar(250) NOT NULL,
+  `status` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
