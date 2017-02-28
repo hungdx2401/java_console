c02328068f4b5697dd7852350059b06b5da55dd0
Thang <anhthang3011@gmail.com>
2017-02-28 10:15:54 +0700

    Tao method update trong products model

--- src/console/java/model/ProductModels.java
+++ src/console/java/model/ProductModels.java
@@ -55,7 +55,7 @@ public class ProductModels {
             String update = String.format(updateQuery,product.getName(),product.getDescription(),product.getQuantity(),product.getPrice(),product.getCategoryId());
             Statement stt = DAO.getConnection().createStatement();
             stt.execute(update);
-            System.out.println("Update thanh cong roi!!!");
+            System.out.println("Update thanh cong !!!");
         } catch (Exception e) {
             System.err.println("Da xay ra loi !!!");
         }
