6468936c021943e9aa23fceade3e406b33de188b
Donghdd00342 <donghdd00342@fpt.edu.vn>
2017-03-02 10:33:52 +0700

    fix views/MainMenu.java

--- src/console/java/views/MainMenu.java
+++ src/console/java/views/MainMenu.java
@@ -21,15 +21,19 @@ public class MainMenu {
             System.out.println("2.Menu admins");
             System.out.println("3.Thoat");
             System.out.println("-----------------------------------------");
-            System.out.println("Chon : ");
-            int choice = ScannerUtilities.getInt();
-            if(choice == 1){
+            int choice;
+            do {
+                System.out.println("Vui lòng chọn (1/2/3) : ");
+                choice = ScannerUtilities.getInt();
+            } while (!((choice == 1) || (choice == 2) || (choice == 3)));
+
+            if (choice == 1) {
                 ProductsViews.productsMenu();
             }
-            if(choice == 2){
+            if (choice == 2) {
                 AdminViews.menuAdmin();
             }
-            if(choice == 3){
+            if (choice == 3) {
                 break;
             }
         }
