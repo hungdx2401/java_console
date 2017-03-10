5d25943eb76984a27c7dbbec5cda94bf909c1180
Donghdd00342 <donghdd00342@fpt.edu.vn>
2017-03-10 11:48:29 +0700

    dùng bảng admin (ko s)

--- src/console/java/seeds/AdminSeed.java
+++ src/console/java/seeds/AdminSeed.java
@@ -19,15 +19,15 @@ public class AdminSeed {
 	  // Database: online_shop
 	  Statement stt = DAO.getConnection().createStatement();
 	  // Xóa dữ liệu cũ
-	  stt.execute("TRUNCATE TABLE admins;");
+	  stt.execute("TRUNCATE TABLE admin;");
 	  // Thêm dữ liệu mẫu
-	  stt.execute("INSERT INTO admins (name, email, pass) VALUES ('admin','admin@admin.com','admin');");
-	  stt.execute("INSERT INTO admins (name, email, pass) VALUES ('guest','guest@admin.com','guest');");
-	  stt.execute("INSERT INTO admins (name, email, pass) VALUES ('dong','dong@admin.com','1');");
-	  stt.execute("INSERT INTO admins (name, email, pass) VALUES ('hung','hung@admin.com','1');");
-	  stt.execute("INSERT INTO admins (name, email, pass) VALUES ('thang','thang@admin.com','1');");
-	  stt.execute("INSERT INTO admins (name, email, pass) VALUES ('truong','truong@admin.com','1');");
-	  stt.execute("INSERT INTO admins (name, email, pass) VALUES ('quang','quang@admin.com','1');");
+	  stt.execute("INSERT INTO admin (name, email, pass) VALUES ('admin','admin@admin.com','admin');");
+	  stt.execute("INSERT INTO admin (name, email, pass) VALUES ('guest','guest@admin.com','guest');");
+	  stt.execute("INSERT INTO admin (name, email, pass) VALUES ('dong','dong@admin.com','1');");
+	  stt.execute("INSERT INTO admin (name, email, pass) VALUES ('hung','hung@admin.com','1');");
+	  stt.execute("INSERT INTO admin (name, email, pass) VALUES ('thang','thang@admin.com','1');");
+	  stt.execute("INSERT INTO admin (name, email, pass) VALUES ('truong','truong@admin.com','1');");
+	  stt.execute("INSERT INTO admin (name, email, pass) VALUES ('quang','quang@admin.com','1');");
 	  System.out.println("------ Chèn dữ liệu mẫu thành công! ------");
 	  // Đăng nhập với tài khoản: admin/admin hoặc guest/guest
      }
